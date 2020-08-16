1063d85807a1f41868a5f11b64e622f45a17ea2f
Hector <hector.ruzicki@globant.com>
2020-08-15 21:44:58 -0300

    cleaned code inside app component

--- top-posts-app/src/app/app.component.spec.ts
+++ top-posts-app/src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'top-posts-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('top-posts-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('top-posts-app app is running!');
-  });
 });
